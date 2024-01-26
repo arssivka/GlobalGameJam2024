882031f2b40a3eb7c9ed7c9a5deb7b1db9533707
Zmilliy <a.as.glukhov@gmail.com>
2024-01-27 00:22:26 +0200

    added jumping

--- Characters/PlayerHero/Hero.cs
+++ Characters/PlayerHero/Hero.cs
@@ -6,65 +6,117 @@ public partial class Hero : CharacterBody3D
 	[Export]
 	public const float Speed = 500.0f;
 	[Export]
-	public const float JumpVelocity = 4.5f;
+	public const float JumpVelocity = 4f;
+	[Export]
+	public const float Gravity = 10f;
+	[Export]
+	public const float GroundedHeight = 0.5f;
+	[Export]
+	public const float JumpStartHeight = 0.1f;
+	private float VerticalSpeed = 0.0f;
 
-	private Vector3 MoveVelocity = new Vector3(0, 0, 0);
+	private Vector3 MoveDirection = new Vector3(0, 0, 0);
 	private NodePath RunMeshPath = new NodePath("Pivot/Hero_Run");
 	private NodePath IdleMeshPath = new NodePath("Pivot/Hero_Idle");
 	private NodePath TickleMeshPath = new NodePath("Pivot/Hero_Tickle");
 	private NodePath CameraPath = new NodePath("CameraPivot/Camera");
 
-	public override void _Ready()
+	private void UpdateDirection()
 	{
-		base._Ready();
+		if (!MoveDirection.IsZeroApprox())
+        {
+			Vector3 direction = -Velocity;
+			direction.Y = 0;
+			if (!direction.IsZeroApprox())
+			{
+				GetNode<Node3D>("Pivot").Basis = Basis.LookingAt(direction);
+			}
+        }
 	}
 
-	public override void _Process(double delta)
+	private void UpdateMovementMeshes(double delta)
 	{
-		base._Process(delta);
-		GetNode<Node3D>(TickleMeshPath).Hide();
-		if (MoveVelocity.IsZeroApprox())
+		if (MoveDirection.IsZeroApprox())
 		{
 			GetNode<Node3D>(RunMeshPath).Hide();
 			GetNode<Node3D>(IdleMeshPath).Show();
 		}
 		else
 		{
-			GetNode<Node3D>(IdleMeshPath).Hide();
-			GetNode<Node3D>(RunMeshPath).Show();
+			Vector3 pivotPosition = GetNode<Node3D>("Pivot").Position; 
+			if (pivotPosition.Y < GroundedHeight)
+			{
+				GetNode<Node3D>(RunMeshPath).Hide();
+				GetNode<Node3D>(IdleMeshPath).Show();
+			}
+			else
+			{
+				GetNode<Node3D>(IdleMeshPath).Hide();
+				GetNode<Node3D>(RunMeshPath).Show();
+			}
 		}
+	}
+
+	private void UpdateJumping(double delta)
+	{
+		Vector3 pivotPosition = GetNode<Node3D>("Pivot").Position; 
+		if (!MoveDirection.IsZeroApprox())
+		{
+			if (pivotPosition.Y < JumpStartHeight)
+			{
+				VerticalSpeed = JumpVelocity;
+			}
+		}
+		VerticalSpeed = Mathf.Max(-JumpVelocity, VerticalSpeed - Gravity * (float)delta);
+
+		pivotPosition.Y = Mathf.Max(0, pivotPosition.Y + VerticalSpeed * (float)delta);
+
+		GetNode<Node3D>("Pivot").Position = pivotPosition;
+	}
+
+	public override void _Ready()
+	{
+		base._Ready();
+		GetNode<Node3D>(TickleMeshPath).Hide();
+	}
+
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
+
 	}
 
 	private void ListenInput()
 	{
 		if (Input.IsActionPressed("RunUp"))
 		{
-			MoveVelocity.Z = -1;
+			MoveDirection.Z = -1;
 		}
 		else if (Input.IsActionPressed("RunDown"))
 		{
-			MoveVelocity.Z = 1;
+			MoveDirection.Z = 1;
 		}
 		else
 		{
-			MoveVelocity.Z = 0;			
+			MoveDirection.Z = 0;			
 		}
 
 		if (Input.IsActionPressed("RunRight"))
 		{
-			MoveVelocity.X = 1;
+			MoveDirection.X = 1;
 		}
 		else if (Input.IsActionPressed("RunLeft"))
 		{
-			MoveVelocity.X = -1;
+			MoveDirection.X = -1;
 		}
 		else
 		{
-			MoveVelocity.X = 0;			
+			MoveDirection.X = 0;			
 		}
-		MoveVelocity = MoveVelocity.Normalized();
-	}
 
+		MoveDirection = MoveDirection.Normalized();
+
+	}
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
@@ -72,41 +124,11 @@ public partial class Hero : CharacterBody3D
 	public override void _PhysicsProcess(double delta)
 	{
 		ListenInput();
-		Velocity = MoveVelocity * Speed * (float)delta;
-
-		// Leave it to better time
-		/*
-		Vector3 velocity = Velocity;
-
-		// Add the gravity.
-		if (!IsOnFloor())
-			velocity.Y -= gravity * (float)delta;
-
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-			velocity.Y = JumpVelocity;
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-		if (direction != Vector3.Zero)
-		{
-			velocity.X = direction.X * Speed;
-			velocity.Z = direction.Z * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
-		}
-
-		Velocity = velocity;
-		*/
+		UpdateMovementMeshes(delta);
+		UpdateDirection();
+		UpdateJumping(delta);
+		Vector3 moveVelocity = MoveDirection * Speed * (float)delta;
+		Velocity = moveVelocity;
 		MoveAndSlide();
-		if (!MoveVelocity.IsZeroApprox())
-        {
-            GetNode<Node3D>("Pivot").Basis = Basis.LookingAt(-Velocity);
-        }
 	}
 }
