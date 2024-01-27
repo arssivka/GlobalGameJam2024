0b82cbeedfda4da9ce9e500466f9482c0b83cfab
Vanya <Vanya@vanya.vanya>
2024-01-28 01:23:25 +0200

    Level_00 lighting, Lamp

---
+++ Art/LD/Lamp.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public partial class Lamp : Node3D
+{
+	[Export] public Color LampColor;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		GetNode<OmniLight3D>("OmniLight3D").LightColor = LampColor;
+		var m = GetNode<MeshInstance3D>("MeshInstance3D").GetSurfaceOverrideMaterial(0);
+		if (m != null)
+		{
+			GD.Print("Material override found!");
+		}
+		
+			// .MaterialOverride.Set("Emission", LampColor);
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
