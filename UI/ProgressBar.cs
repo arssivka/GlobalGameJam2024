ace7151d7a14ca5459f1a43763809bdba52cfbb4
Nikita Elsakov <vocasle@gmail.com>
2024-01-27 22:05:01 +0200

    Add ProgressBar scene

---
+++ UI/ProgressBar.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public partial class ProgressBar : Control
+{
+	public override void _Ready()
+	{
+		base._Ready();
+	}
+
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
+	}
+}
