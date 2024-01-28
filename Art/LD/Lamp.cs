c9305c98f328f3c3349f2fe4d731340a991e1174
Zmilliy <a.as.glukhov@gmail.com>
2024-01-28 13:57:33 +0200

    fixed lamps

--- Art/LD/Lamp.cs
+++ Art/LD/Lamp.cs
@@ -1,16 +1,26 @@
 using Godot;
 using System;
+using System.Drawing;
 
 public partial class Lamp : Node3D
 {
-	[Export] public Color LampColor;
+	[Export] public bool IsBlue = false;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		GetNode<OmniLight3D>("OmniLight3D").LightColor = LampColor;
-		var m = GetNode<MeshInstance3D>("MeshInstance3D").GetSurfaceOverrideMaterial(0) as StandardMaterial3D;
-		m.Emission = LampColor;
+		if (IsBlue)
+		{
+			GetNode<OmniLight3D>("OmniLight3D").LightColor = new Godot.Color(Godot.Colors.Blue);
+
+			GetNode<MeshInstance3D>("MeshInstanceRed").Hide();
+			GetNode<MeshInstance3D>("MeshInstanceBlue").Show();
+		}
+		else
+		{
+			GetNode<MeshInstance3D>("MeshInstanceRed").Show();
+			GetNode<MeshInstance3D>("MeshInstanceBlue").Hide();
+		}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
