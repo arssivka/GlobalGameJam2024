a84dbb6f7ca8ba69fdad113567eaedb2993871e6
Nikita Elsakov <vocasle@gmail.com>
2024-01-28 11:54:26 +0200

    Change color of the lamp via color picker

--- Art/LD/Lamp.cs
+++ Art/LD/Lamp.cs
@@ -9,13 +9,8 @@ public partial class Lamp : Node3D
 	public override void _Ready()
 	{
 		GetNode<OmniLight3D>("OmniLight3D").LightColor = LampColor;
-		var m = GetNode<MeshInstance3D>("MeshInstance3D").GetSurfaceOverrideMaterial(0);
-		if (m != null)
-		{
-			GD.Print("Material override found!");
-		}
-		
-			// .MaterialOverride.Set("Emission", LampColor);
+		var m = GetNode<MeshInstance3D>("MeshInstance3D").GetSurfaceOverrideMaterial(0) as StandardMaterial3D;
+		m.Emission = LampColor;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
